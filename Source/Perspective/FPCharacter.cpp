2a5ff491135ac84c615dbe58e1c40dac430ff60e
Mikel <mike.srom@yahoo.com>
2019-01-14 17:31:30 -0500

    Character Movement and Camera
    Finished movement and camera for character

--- Source/Perspective/FPCharacter.cpp
+++ Source/Perspective/FPCharacter.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPCharacter.h"
+#include "Perspective.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 AFPCharacter::AFPCharacter()
@@ -15,6 +17,11 @@ void AFPCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+	}
+
+	maxWalkSpeed = GetCharacterMovement()->MaxWalkSpeed;
 }
 
 // Called every frame
@@ -29,5 +36,52 @@ void AFPCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	//Players movement bindings.
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFPCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPCharacter::MoveRight);
+
+	//Players look bindings.
+	PlayerInputComponent->BindAxis("Turn", this, &AFPCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &AFPCharacter::AddControllerPitchInput);
+
+	//Players jump bindings.
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFPCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AFPCharacter::StopJump);
+
+	PlayerInputComponent->BindAction("Run", IE_Pressed, this, &AFPCharacter::StartSprint);
+	PlayerInputComponent->BindAction("Run", IE_Released, this, &AFPCharacter::StopSprint);
+
 }
 
+void AFPCharacter::MoveForward(float Value)
+{
+	// Find out which way is "forward" and record that the player wants to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, Value);
+}
+
+void AFPCharacter::MoveRight(float Value)
+{
+	// Find out which way is "right" and record that the player wants to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, Value);
+}
+
+void AFPCharacter::StartJump() {
+	bPressedJump = true;
+}
+
+void AFPCharacter::StopJump() {
+	bPressedJump = false;
+}
+
+void AFPCharacter::StartSprint() {
+	
+	GetCharacterMovement()->MaxWalkSpeed = maxWalkSpeed * sprintModifier;
+}
+
+void AFPCharacter::StopSprint() {
+
+	GetCharacterMovement()->MaxWalkSpeed = maxWalkSpeed;
+
+}
--- Source/Perspective/FPCharacter.h
+++ Source/Perspective/FPCharacter.h
@@ -2,15 +2,18 @@
 
 #pragma once
 
+#include "Engine.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "FPCharacter.generated.h"
+
 
 UCLASS()
 class PERSPECTIVE_API AFPCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
+		
 public:
 	// Sets default values for this character's properties
 	AFPCharacter();
@@ -18,7 +21,6 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -26,4 +28,31 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	//Handles forward and backward movement
+	UFUNCTION()
+		void MoveForward(float Value);
+
+	// Handles right and left movement.
+	UFUNCTION()
+		void MoveRight(float Value);
+
+	//Handles jumping
+	//Sets jump flag when key is pressed.
+	UFUNCTION()
+		void StartJump();
+
+	// Clears jump flag when key is released.
+	UFUNCTION()
+		void StopJump();
+
+	//Handles player running
+	UFUNCTION()
+		void StartSprint();
+
+	UFUNCTION()
+		void StopSprint();
+	
+private:
+	float maxWalkSpeed = 1.0f;
+	float sprintModifier = 5.0f;
 };
